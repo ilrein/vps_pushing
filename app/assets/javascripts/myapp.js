e65a21b5ca3f8a853a54ce838eb1e370cf664187
Ilia <ilia.reingold@gmail.com>
2015-01-12 21:33:18 -0500

    progress on some animations

--- app/assets/javascripts/myapp.js
+++ app/assets/javascripts/myapp.js
@@ -1,20 +1,43 @@
 var ready;
 ready = function() {
 
-  $('.tlt').textillate({
+  $('.tlttop').textillate({
+   in: { 
+    delay: 1.3,
+    effect: 'bounceIn'
+  }});
+
+  $('.tltleft').textillate({
    in: { 
     delay: 1.3,
     effect: 'fadeInLeftBig'
   }});
 
-  $('i.facebook').on('click', function(){
+  var fademe2 = $('.fademein2');
 
+  TweenLite.set(fademe2, {autoAlpha: 0});
+  TweenLite.to(fademe2, 1.2, {autoAlpha:1}).delay(2);
+
+  var fademe3 = $('.fademein3');
+
+  TweenLite.set(fademe3, {autoAlpha: 0});
+  TweenLite.to(fademe3, 1.2, {autoAlpha:1}).delay(3);
+
+  var tl = new TimelineLite;
+  var gasmask = $('#gasmask');
+  tl.add(TweenLite.to(gasmask, 0.4, { 
+    boxShadow: "5px 5px 2.5px 0px rgba(0,0,0,0.75)",
+    ease: Linear.easeNone
+  }));
+
+
+  $('#gasmask').on('mouseenter', function(){
+   tl.play(); 
   });
 
-  var fademe = $('.fademein');
-
-  TweenLite.set(fademe, {autoAlpha: 0});
-  TweenLite.to(fademe, 1.2, {autoAlpha:1}).delay(2);
+  $('#gasmask').on('mouseleave', function(){
+    tl.reverse();
+  });
 
 };
 
