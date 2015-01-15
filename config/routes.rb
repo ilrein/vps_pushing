cdd42846f886a036c47358486cb6b6ec1ccfe6e8
Ilia Reingold <ilia.reingold@gmail.com>
2015-01-15 04:28:30 +0000

    omg i got it lol

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Myapp::Application.routes.draw do
   get "home/index"
   root to: 'home#index'
-  resource :design, controller: :home
+  resource :designs, controller: :home
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
